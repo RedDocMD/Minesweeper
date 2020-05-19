184c16c06c81a06c84861b7e614f7d2c6e739a4b
Deep Majumder <deep.majumder2019@gmail.com>
2020-05-19 23:41:00 +0530

    initComponent() of MinesweeperBoard made

--- src/com/deep/minesweeper/gui/MinesweeperBoard.java
+++ src/com/deep/minesweeper/gui/MinesweeperBoard.java
@@ -3,19 +3,33 @@ package com.deep.minesweeper.gui;
 import com.deep.minesweeper.data.MinesweeperBoardData;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MinesweeperBoard extends JComponent {
     private final MinesweeperBoardData boardData;
     private final JButton[][] cells;
+    private final JPanel backPanel;
+    private final int rows;
+    private final int columns;
 
     public MinesweeperBoard(MinesweeperBoardData boardData) {
         super();
         this.boardData = boardData;
-        this.cells = new JButton[boardData.getRows()][boardData.getColumns()];
+        this.rows = boardData.getRows();
+        this.columns = boardData.getColumns();
+        this.cells = new JButton[rows][columns];
+        this.backPanel = new JPanel();
         initComponent();
     }
 
     private void initComponent() {
         // TODO: Initialize layout and listeners
+        backPanel.setLayout(new GridLayout(rows, columns));
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < columns; j++) {
+                cells[i][j] = new JButton();
+                backPanel.add(cells[i][j]);
+            }
+        }
     }
 }
